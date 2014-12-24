e19db237aab5c6cafcbba9746ff3ea010e499dac
gl2748 <imaitland@gmail.com>
2014-12-24 09:00:17 -0500

    added init dbconfig script

---
+++ ohana-api-dockerfile/bin/dbconfig.sh
@@ -0,0 +1,10 @@
+#! bin/bash
+#Point the ohana container to the postgresql container
+#edit ~/ohana/config/database.yml so that it points to the postgresql container
+#NB - this script needs to be executable i.e. sudo chmod a+x dbconfig.sh
+
+
+newhost="$echo $DB_PORT_5432_TCP_ADDR"
+oldhost="localhost"
+sudo sed -i 's/'"$oldhost"'/'"$newhost"'/g' home/ruby/ohana/config/database.yml
+
