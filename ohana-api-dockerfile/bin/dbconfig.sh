c679182577f8eabe8e66da76197130fbf8477523
gl2748 <imaitland@gmail.com>
2014-12-24 09:56:33 -0500

    minor edit

--- ohana-api-dockerfile/bin/dbconfig.sh
+++ ohana-api-dockerfile/bin/dbconfig.sh
@@ -1,10 +1,8 @@
 #! bin/bash
-#Point the ohana container to the postgresql container
 #edit ~/ohana/config/database.yml so that it points to the postgresql container
 #NB - this script needs to be executable i.e. sudo chmod a+x dbconfig.sh
 
-
 newhost="$echo $DB_PORT_5432_TCP_ADDR"
 oldhost="localhost"
-sudo sed -i 's/'"$oldhost"'/'"$newhost"'/g' home/ruby/ohana/config/database.yml
+sudo sed -i 's/'"$oldhost"'/'"$newhost"'/g' /home/ruby/ohana/config/database.yml
 
