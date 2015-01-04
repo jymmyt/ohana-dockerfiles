55d218fce4a00eaace44cd7f87bc0265463fc336
gl2748 <imaitland@gmail.com>
2015-01-04 06:34:41 -0500

    added leading slashes

--- ohana-api-dockerfile/bin/dbconfig.sh
+++ ohana-api-dockerfile/bin/dbconfig.sh
@@ -1,11 +1,11 @@
-#! bin/bash
+#! /bin/bash
 #edit ~/ohana/config/database.yml so that it points to the postgresql container
 #NB - this script needs to be executable i.e. sudo chmod a+x dbconfig.sh
 
 newhost="$echo $DB_PORT_5432_TCP_ADDR"
 oldhost="localhost"
-sed -i 's/'"$oldhost"'/'"$newhost"'/g' home/ohana1/config/database.yml
+sed -i 's/'"$oldhost"'/'"$newhost"'/g' /home/ohanauser/ohana/config/database.yml
 
 newport="$echo $DB_PORT_5432_TCP_PORT"
 oldport="5432"
-sed -i 's/'"$oldport"'/'"$newport"'/g' home/ohana1/config/database.yml
+sed -i 's/'"$oldport"'/'"$newport"'/g' /home/ohanauser/ohana/config/database.yml
